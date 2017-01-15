d5928341092e4b225450394406906df390af477c
maribel <you@example.com>
2017-01-15 16:10:40 -0500

    javascript terminado

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var Ingresa= document.getElementById("form-signin-heading");
+	Ingresa.innerHTML="Por favor inicia sesión";
+var enterMail=document.getElementById("inputEmail").placeholder="Correo Electrónico";
+	var enterPassword=document.getElementById("inputPassword").placeholder="Contraseña";
+	var recordar = document.querySelector("span").innerHTML="Recordar datos";
+	var recordar = document.querySelector("button").innerHTML="Iniciar Sesión";
+	
